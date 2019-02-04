a908d6507d84e4cb92009b14ed9c815936454cda
JP <bonnerjp@gmail.com>
2019-02-03 23:34:38 -0800

    bugfix for loop, add parser

--- game.py
+++ game.py
@@ -3,8 +3,29 @@ import sys
 import json
 
 
+class Parser:
+    class __Parser:
+        def __init__(self, arg):
+            self.val = arg
+
+        def __str__(self):
+            return repr(self) + self.val
+
+    instance = None
+
+    def __init__(self, arg):
+        if not Parser.instance:
+            Parser.instance = Parser.__Parser(arg)
+        else:
+            Parser.instance.val = arg
+
+    def __getattr__(self, name):
+        return getattr(self.instance, name)
+
+
 class Player:
     def __init__(self, name=''):
+        print('__init__')
         """player stats
         hp?
         location?
@@ -14,6 +35,7 @@ class Player:
         self.name = name
 
     def is_alive(self):
+        print('is_alive')
         self.hp > 0
 
 
@@ -27,23 +49,28 @@ class Block:
     DEFAULT_DESCRIPTION = 'You are in a converted hotel above a laundromat. You are likely to be eaten by a grue.'
 
     def __init__(self, surface='WALL', description=DEFAULT_DESCRIPTION):
+        print('__init__')
         self.surface = surface
-        self.description = description
+        self.description = description or self.DEFAULT_DESCRIPTION
 
     def __str__(self):
+        print('__str__')
         self.description
 
 
 class GridGraph:
     def __init__(self):
+        print('__init__')
         self.width = 0
         self.height = 0
         self.nodes = {}
 
     def is_boundary(self, location):
+        print('is_boundary')
         return len(self.neighbors(location)) < 4
 
     def neighbors(self, location):
+        print('neighbors')
         neighbors = [
             self.nodes.get(location[0] - 1, location[1]),
             self.nodes.get(location[0] + 1, location[1]),
@@ -53,11 +80,14 @@ class GridGraph:
         return filter(lambda n: n, neighbors)
 
     def insert_node(self, location, **kwargs):
+        print('insert_node')
         if location[0] > self.width:
             self.width = location[0]
         if location[1] > self.height:
             self.height = location[1]
-        self.nodes[location] = Block(kwargs.get('surface'), kwargs.get('description'))
+        self.nodes[location] = Block(
+                                     kwargs.get('surface'),
+                                     kwargs.get('description'))
 
 
 class Game:
@@ -69,6 +99,7 @@ class Game:
     }
 
     def __init__(self, initial_state):
+        print('__init__')
         """Set up some useful state.
         """
         self.current_coords = (0, 0)
@@ -79,6 +110,7 @@ class Game:
             self._read(aspect, initial_state[aspect])
 
     def _read(self, state_aspect, data):
+        print('_read')
         """Translate state aspect e.g. game map into game
         """
         if state_aspect == 'player':
@@ -108,6 +140,7 @@ class Game:
             self.content = data
 
     def _draw(self):
+        print('_draw')
         unmap = {v: k for k, v in self.MAP_KEY.items()}
         line_buffer = ''
         for y in range(0, self.map.height + 1):
@@ -118,22 +151,26 @@ class Game:
             line_buffer = ''
 
     def _introduction(self):
+        print('_introduction')
         self._narrate(self.content.get("introduction"))
         self.player.name = self._ask("What's your name again?")
         print("OK, %s." % self.player.name)
 
     def _parse(self, input):
+        print('_parse')
         """Match player input to something
         """
         USELESS_WORDS = ['a', 'the', 'i']
         return input
 
     def _describe(self):
+        print('_describe')
         print(self.current_coords)
         surroundings = self.map.nodes.get(self.current_coords)
         self._narrate(surroundings.description)
 
     def _narrate(self, content):
+        print('_narrate')
         print()
         print('* ' * 20)
         for char in content:
@@ -149,6 +186,7 @@ class Game:
         print()
 
     def _ask(self, prompt=None):
+        print('_ask')
         NO_COMPRENDE = [
             'No comprende. Uno mas?',
             "Sorry, didn't get it. Use smaller words, please.",
@@ -165,11 +203,13 @@ class Game:
         return action
 
     def _respond(self, action):
+        print('_respond')
         """Apply result here, increment/decrement whatever
         """
         self._narrate("You chose to %s. That's a bold move." % action)
 
     def play(self):
+        print('play')
         """Do loop for a) prompting based on state b) translating action c) mutating state based on action d) presenting result
         """
         if not self.player.name:
@@ -178,7 +218,7 @@ class Game:
         while self.player.is_alive:
             self._describe()
             action = self._ask()
-            self._respond()
+            self._respond(action)
 
 
 game = Game({
